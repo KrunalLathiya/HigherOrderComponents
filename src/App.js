47d42e1a953b267cc4dd49be90fbfc6f1bdbbb1c
KrunalLathiya <krunallathiya21@gmail.com>
2017-10-25 20:11:58 +0530

    Higher-Order Component

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import StockList from './StockList';
+import UserList from './UserList';
+import Hoc from './HOC';
+
+const StocksData = [
+  {
+      id: 1,
+      name: 'TCS'
+        
+  },
+  {
+      id: 2,
+      name: 'Infosys'
+  },
+  {
+      id: 3,
+      name: 'Reliance'
+  }
+];
+const UsersData = [
+  {
+      id: 1,
+      name: 'Krunal'
+        
+  },
+  {
+      id: 2,
+      name: 'Ankit'
+  },
+  {
+      id: 3,
+      name: 'Rushabh'
+  }
+];
+
+const Stocks = Hoc(
+  StockList,
+  StocksData
+);
+
+const Users = Hoc(
+  UserList,
+  UsersData
+);
+
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <Stocks></Stocks>
+      </div>
+    )
+  }
+}
+
+export default App;
